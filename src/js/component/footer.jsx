3f900dd4615bce62cc4475df09400349e45adedb
crduque <crduque@hotmail.com>
2020-10-20 20:08:16 +0000

    Aded bg and text color

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = props => {
 	let myFooter = (
-		<p className="bg-secondary">Copyright of your website, 2020</p>
+		<p className="text-white p-2">Copyright of your website, 2020</p>
 	);
 	return myFooter;
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Footer from "./footer.jsx";
+import { Footer } from "./footer.jsx";
 
 //create your first component
 export function Home() {
 	let footerTag = (
-		<footer className="text-center mt-5">
+		<footer className="text-center bg-secondary">
 			<Footer footerContent="Hello I'm the text of the footer" />
 		</footer>
 	);
