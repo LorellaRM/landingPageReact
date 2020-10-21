d099d9faabea2ed23795c000ba9dca9da027110b
LorellaRM <lorella.1892@gmail.com>
2020-10-21 09:35:44 +0000

    Navbar end

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,16 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types';
 
-
+export function Navbar (props) {
+    return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href={props.urlNB}>{props.navbarName}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
-      <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-      <a className="nav-item nav-link" href="#">Features</a>
-      <a className="nav-item nav-link" href="#">Pricing</a>
-      <a className="nav-item nav-link disabled" href="#">Disabled</a>
+      <a className="nav-item nav-link active" href={props.urlHome}>{props.home} <span className="sr-only">(current)</span></a>
+      <a className="nav-item nav-link" href={props.url1}>{props.opt1}</a>
+      <a className="nav-item nav-link" href={props.url2}>{props.opt2}</a>
+      <a className="nav-item nav-link" href={props.url3}>{props.opt3}</a>
     </div>
   </div>
-</nav>
+</nav>)
+}
+
+Navbar.propTypes = {
+  navbarName: PropTypes.string,
+  urlNB: PropTypes.string,
+  url1: PropTypes.string,
+  url2: PropTypes.string,
+  url3: PropTypes.string,
+  urlHome: PropTypes.string,
+  home: PropTypes.string,
+  opt1: PropTypes.string,
+  opt2: PropTypes.string,
+  opt3: PropTypes.string,
+}
