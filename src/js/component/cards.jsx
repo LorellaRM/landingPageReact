e89304ea8d09afd65e76eafcbe54e51e23e85f85
crduque <crduque@hotmail.com>
2020-10-21 08:34:08 +0000

    Created four cards with its images

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,3 +1,80 @@
 import React from "react";
 
-export function Card() {}
+export function Cards() {
+	return (
+		<div className="row">
+			{/* CARD 1 */}
+			<div className="card col-3">
+				<img
+					src="https://images.pexels.com/photos/3689976/pexels-photo-3689976.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+					className="card-img-top"
+					alt="#"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">PALETTES</h5>
+					<p className="card-text">
+						Whether you are looking for palettes to make a full
+						color makeup, or if you need one in nudes tones for day
+						to day, right here we have a selection of the
+						best-selling ones in the world.
+					</p>
+					{/* button */}
+				</div>
+			</div>
+			{/* CARD 2 */}
+			<div className="card col-3">
+				<img
+					src="https://images.pexels.com/photos/4041392/pexels-photo-4041392.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+					className="card-img-top"
+					alt="#"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">SKINCARE</h5>
+					<p className="card-text">
+						Whether you are looking for palettes to make a full
+						color makeup, or if you need one in nudes tones for day
+						to day, right here we have a selection of the
+						best-selling ones in the world.
+					</p>
+					{/* button */}
+				</div>
+			</div>
+			{/* CARD 3 */}
+			<div className="card col-3">
+				<img
+					src="https://images.pexels.com/photos/4614/woman-morning-bathrobe-bathroom.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940S"
+					className=""
+					alt="#"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">PALETTES</h5>
+					<p className="card-text">
+						Whether you are looking for palettes to make a full
+						color makeup, or if you need one in nudes tones for day
+						to day, right here we have a selection of the
+						best-selling ones in the world.
+					</p>
+					{/* button */}
+				</div>
+			</div>
+			{/* CARD 4 */}
+			<div className="card col-3">
+				<img
+					src="https://images.pexels.com/photos/2721977/pexels-photo-2721977.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+					className="card-img-top"
+					alt="#"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">PALETTES</h5>
+					<p className="card-text">
+						Whether you are looking for palettes to make a full
+						color makeup, or if you need one in nudes tones for day
+						to day, right here we have a selection of the
+						best-selling ones in the world.
+					</p>
+					{/* button */}
+				</div>
+			</div>
+		</div>
+	);
+}
