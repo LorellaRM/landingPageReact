2582388ede92521f9e9586077bc0254e6d436390
LorellaRM <lorella.1892@gmail.com>
2020-10-21 09:48:10 +0000

    Navbar completed

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,23 +2,24 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar.jsx";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid p-0">
+			<Navbar
+				navbarName="MakeUP"
+				urlNB=""
+				url1="https://www.kikocosmetics.com/es-es/promo-kiko/gift-with-purchase-oct-2020.html"
+				url2=""
+				url3=""
+				urlHome=""
+				home="Home"
+				opt1="Option 1"
+				opt2="Oprion 2"
+				opt3="Oprion 3"
+			/>
 		</div>
 	);
 }
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,33 +1,53 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export function Navbar (props) {
-    return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href={props.urlNB}>{props.navbarName}</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link active" href={props.urlHome}>{props.home} <span className="sr-only">(current)</span></a>
-      <a className="nav-item nav-link" href={props.url1}>{props.opt1}</a>
-      <a className="nav-item nav-link" href={props.url2}>{props.opt2}</a>
-      <a className="nav-item nav-link" href={props.url3}>{props.opt3}</a>
-    </div>
-  </div>
-</nav>)
+export function Navbar(props) {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a className="navbar-brand" href={props.urlNB}>
+				{props.navbarName}
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<a
+						className="nav-item nav-link active"
+						href={props.urlHome}>
+						{props.home} <span className="sr-only">(current)</span>
+					</a>
+					<a className="nav-item nav-link" href={props.url1}>
+						{props.opt1}
+					</a>
+					<a className="nav-item nav-link" href={props.url2}>
+						{props.opt2}
+					</a>
+					<a className="nav-item nav-link" href={props.url3}>
+						{props.opt3}
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 Navbar.propTypes = {
-  navbarName: PropTypes.string,
-  urlNB: PropTypes.string,
-  url1: PropTypes.string,
-  url2: PropTypes.string,
-  url3: PropTypes.string,
-  urlHome: PropTypes.string,
-  home: PropTypes.string,
-  opt1: PropTypes.string,
-  opt2: PropTypes.string,
-  opt3: PropTypes.string,
-}
+	navbarName: PropTypes.string,
+	urlNB: PropTypes.string,
+	url1: PropTypes.string,
+	url2: PropTypes.string,
+	url3: PropTypes.string,
+	urlHome: PropTypes.string,
+	home: PropTypes.string,
+	opt1: PropTypes.string,
+	opt2: PropTypes.string,
+	opt3: PropTypes.string
+};
