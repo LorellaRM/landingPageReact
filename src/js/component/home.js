28483eca6457b0906150a4ddf27f6380579bca37
crduque <crduque@hotmail.com>
2020-10-21 13:12:01 +0000

    Home file with jumbotron added

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HeaderJum } from "./jumbotron.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,8 +8,8 @@ import { Button } from "./button.jsx";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<Button textButton="Click button" classContent="btn btn-primary" />
+		<div className="container col-12">
+			<HeaderJum topTitle="The Best MakeUp" text="New style of live" />
 		</div>
 	);
 }
